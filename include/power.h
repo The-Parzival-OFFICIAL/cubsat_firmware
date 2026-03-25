f5e73cb1648d0a4ffc342599871df5edb48e31c4
greg <57497732+The-Parzival-OFFICIAL@users.noreply.github.com>
2026-03-25 22:11:45 +0530

    hey

--- include/power.h
+++ include/power.h
@@ -1,6 +1,13 @@
 #ifndef POWER_H
 #define POWER_H
 
+// ===============================
+// POWER MODULE (INA219)
+// I2C Pins:
+// SDA -> GPIO21
+// SCL -> GPIO22
+// ===============================
+
 void power_init(void);
 void power_task(void *param);
 
